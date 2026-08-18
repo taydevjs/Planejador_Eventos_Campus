7eaf2fbde1a8f8e92afa77746b3774b0169cc1ae
Usuário Local Riacho Fundo <acessolocal@ifb.local>
2026-08-18 15:05:35 -0300

    adicionei funcao de menu

---
+++ main.py
@@ -0,0 +1,8 @@
+def displayMenu():
+    print("=== Planejador de Eventos do Campus ===")
+    print("1. Adicionar Evento")
+    print("2. Ver Todos os Eventos")
+    print("3. Filtrar por Categoria")
+    print("4. Marcar Evento como Participado")
+    print("5. Gerar Relatório")
+    print("6. Sair")
