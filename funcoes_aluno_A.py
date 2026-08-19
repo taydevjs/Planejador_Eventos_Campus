837229a876adc792f2e65e2a1a92f856677ef761
JOAO VITOR CARDOSO ALVES <joaovitor.calves@a.ucb.br>
2026-08-19 09:50:22 -0300

    ajustes na função de adicionar listas

--- funcoes_aluno_A.py
+++ funcoes_aluno_A.py
@@ -1,8 +1,10 @@
+
 lista_eventos = []
+lista_eventos_geral = []
 
 #função de mostra a lista de eventos
 def mostrar_eventos():
-    print(lista_eventos)
+    print(lista_eventos_geral)
 
 # função para confirmar no enter
 def enter_confirm():
@@ -15,6 +17,24 @@ def enter_confirm():
 # função de dar espaço entre resultados    
 def espaco_vazio():
     print("")
+
+# função para adicionar listas
+def adicionar_evento():
+        adicionar = list()
+        adicionar_evento_nome  = input("\ndigite o nome evento que voce deseja adicionar: ")
+        adicionar_evento_data  = input("\ndigite a data de realização dele: ")
+        adicionar_evento_local  = input("\ndigite o local onde acontecera o evento: ")
+        adicionar_evento_genero  = input("\ndigite qual o tipo de evento: ")
+        adicionar.append(adicionar_evento_nome)
+        adicionar.append(adicionar_evento_data)
+        adicionar.append(adicionar_evento_local)
+        adicionar.append(adicionar_evento_genero)
+        lista_eventos_geral.append(adicionar)
+        espaco_vazio()
+        mostrar_eventos()
+        espaco_vazio()
+        enter_confirm()
+        espaco_vazio()
 
 while True:
     print("1. Mostrar os eventos atuais")
@@ -30,21 +50,15 @@ while True:
         espaco_vazio()
 
     elif opcao == '2':
-        espaco_vazio()
-        adicionar_evento  = input("\ndigite o evento que voce deseja adicionar: ")
-        lista_eventos.append(adicionar_evento)
-        espaco_vazio()
-        mostrar_eventos()
-        espaco_vazio()
-        enter_confirm()
-        espaco_vazio()
+        adicionar_evento()
 
     elif opcao == '3':
         print("\ntchau :)\n")
         break
     
     else:
-        print("voce nao digitou nenhum carctere disponivel")
-        break
+        espaco_vazio()
+        print("voce nao digitou nenhum carctere disponivel, tente novamente")
+        espaco_vazio()
     
 
