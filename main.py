20710852eac356085f2d168bca06c1fafa9ab60a
Usuário Local Riacho Fundo <acessolocal@ifb.local>
2026-08-25 16:32:47 -0300

    deletar e procurar por nome

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def espaco_vazio():
 # funcao para confirmar no enter
 def enter_confirm():
     verificar_enter = input("digite ENTER para continuar...")
-    if verificar_enter == ' ':
+    if verificar_enter == '':
         return 0
     else:
         print("Voce nao apertou enter")
@@ -21,10 +21,12 @@ def displayMenu():
     print("=== Planejador de Eventos do Campus ===")
     print("1. Adicionar Evento")
     print("2. Ver Todos os Eventos")
-    print("3. Filtrar por Categoria")
-    print("4. Marcar Evento como Participado")
-    print("5. Gerar Relatório")
-    print("6. Sair ")
+    print("3. Pesquisar Eventos por Nome")
+    print("4. Filtrar por Categoria")
+    print("5. Marcar Evento como Participado")
+    print("6. Deletar Evento")
+    print("7. Gerar Relatório")
+    print("8. Sair ")
 
 def menu_principal():
 
@@ -38,8 +40,8 @@ def menu_principal():
         if opcoes == '1':
             limpar_tela()
 
-            nome , data, local , genero = funcoes_aluno_A.lerValoresEvento()
-            funcoes_aluno_A.adicionarEvento(funcoes_aluno_A.lista_eventos, nome, data , local , genero)
+            nome, data, local, genero = funcoes_aluno_A.lerValoresEvento()
+            funcoes_aluno_A.adicionarEvento(funcoes_aluno_A.lista_eventos, nome, data, local, genero)
 
             espaco_vazio()
             enter_confirm()
@@ -54,9 +56,12 @@ def menu_principal():
             enter_confirm()
             espaco_vazio()
 
-        elif opcoes == '2':
+        elif opcoes == '3':
             limpar_tela()
-            print("teste03")
+            espaco_vazio()
+            funcoes_aluno_A.pesquisaPorNomeEventos(funcoes_aluno_A.lista_eventos)
+            espaco_vazio()
+
         elif opcoes == '4':
             limpar_tela()
             print("teste04")
@@ -67,7 +72,16 @@ def menu_principal():
 
         elif opcoes == '6':
             limpar_tela()
+            funcoes_aluno_A.deletarEvento(funcoes_aluno_A.lista_eventos)
             print("teste06")
+
+        elif opcoes == '7':
+            limpar_tela()
+            print("teste07")
+
+        elif opcoes == '8':
+            limpar_tela()
+            print("teste08")
         
         else:
             limpar_tela()
